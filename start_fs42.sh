451f7d665b38d07a17c6154b0fbd35c5fecfd34b
lionsarmor <james.m.weeks@gmail.com>
2025-06-07 10:31:50 -0600

    changes to start script.

--- start_fs42.sh
+++ start_fs42.sh
@@ -3,7 +3,8 @@ sleep 5  # Optional, helps on slower boots
 cd /mnt/fs42drive/FieldStation42 || exit 1
 source env/bin/activate
 DISPLAY=:0 
-python3 field_player.py &
-python3 fs42/change_channel.py &
+python3 field_player.py 
+python3 fs42/change_channel.py
+sleep 1 
 python3 fs42/osd/main.py
 
